87c4eff2fd6e63b8a173f7e402fa29be7085b921
nine_swords <nine_swords@gmail.com>
2023-10-11 21:29:01 +0800

    add SConstruct and compiler.py

---
+++ STM32F4-LED/compiler.py
@@ -0,0 +1,50 @@
+import os
+
+# board options
+BOARD = 'STM32F407G-DISC1'
+
+# toolchains options
+ARCH = 'arm'
+CPU = 'cortex-m4'
+FPU = 'fpv4-sp-d16'
+FLOAT_ABI = 'hard'
+# build version: debug or release
+BUILD = 'release'
+
+# toolchains
+PREFIX = 'arm-none-eabi-'
+CC = PREFIX + 'gcc'
+AS = PREFIX + 'gcc'
+AR = PREFIX + 'ar'
+CXX = PREFIX + 'g++'
+LINK = PREFIX + 'gcc'
+TARGET_EXT = 'elf'
+SIZE = PREFIX + 'size'
+OBJDUMP = PREFIX + 'objdump'
+OBJCPY = PREFIX + 'objcopy'
+
+MCU = ' -mcpu=' + CPU + \
+    ' -mthumb -mfpu=' + FPU + ' -mfloat-abi=' + FLOAT_ABI +' -ffunction-sections -fdata-sections'
+
+DEFINES = ' -DUSE_HAL_DRIVER -DSTM32F407xx'
+
+CFLAGS = MCU + \
+    ' -g -Wall -Wstrict-aliasing=0 -Wno-uninitialized -Wno-unused-function -Wno-switch' + DEFINES
+AFLAGS = ' -c' + MCU + ' -x assembler-with-cpp -Wa,-mimplicit-it=thumb '
+LFLAGS = MCU + ' -lm -lgcc -lc' + \
+    ' -nostartfiles -Wl,--gc-sections,--print-memory-usage,-Map=build/' + BOARD + '.map,-cref,-u,Reset_Handler -T link.lds'
+
+CPATH = ''
+LPATH = ''
+
+if BUILD == 'debug':
+    CFLAGS += ' -O0 -gdwarf-2'
+    AFLAGS += ' -gdwarf-2'
+else:
+    CFLAGS += ' -O2'
+
+CXXFLAGS = CFLAGS
+CFLAGS += ' -std=c99'
+CXXFLAGS += ' -std=c++14'
+
+POST_ACTION = OBJCPY + ' -O binary $TARGET build/' + BOARD + '.bin\n' + SIZE + ' $TARGET \n'
